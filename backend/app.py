d4d22690a212c3f60262b2d05b4964146e2bdea3
Omkar Lakhute <omkarlakhutework2@gmail.com>
2025-02-22 22:44:30 +0530

    updates

--- backend/app.py
+++ backend/app.py
@@ -323,4 +323,4 @@ if __name__ == '__main__':
     # Use the PORT environment variable (set by Railway) and host '0.0.0.0'
     port = int(os.environ.get("PORT", 5000))
     print("Starting app on port", port)
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=False, host="0.0.0.0", port=port)
